67947409362620ed28a63f5f3c63da48a5006f22
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2023-12-07 13:40:34 +0530

    added concurrency to the echo server

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,25 @@ use tokio::{
 #[tokio::main]
 async fn main() {
     let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-    let (read, mut writer) = socket.split();
-
-    let mut reader: BufReader<ReadHalf> = BufReader::new(read);
-    let mut line: String = String::new();
 
     loop {
-        let bytes_read: usize = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 {
-            break;
-        }
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        writer.write_all(&line.as_bytes()).await.unwrap();
-        line.clear();
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader: BufReader<ReadHalf> = BufReader::new(reader);
+            let mut line: String = String::new();
+
+            loop {
+                let bytes_read: usize = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+
+                writer.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
